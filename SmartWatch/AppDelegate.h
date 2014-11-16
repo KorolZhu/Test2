fe70fc51e56aced9cc5b03319097c8c48e94650e
zhuzhi <zhuzhi2013@gmail.com>
2014-11-16 14:18:31 +0800

    Initial Commit

---
+++ SmartWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartWatch
+//
+//  Created by zhuzhi on 14/11/16.
+//  Copyright (c) 2014年 SW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartWatch/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  SmartWatch
+//
+//  Created by zhuzhi on 14/11/16.
+//  Copyright (c) 2014年 SW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
